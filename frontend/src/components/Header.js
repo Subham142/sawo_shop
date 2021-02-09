26a9851e2331be349a50384f7be7ae2642f46446
moulina pradhan <pmoulina76@gmail.com>
2021-02-09 20:48:57 +0530

    add footer text

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
         <Container>
         <LinkContainer to="/">
-  <Navbar.Brand >Shop Buddy</Navbar.Brand>
+  <Navbar.Brand >SawoShop</Navbar.Brand>
   </LinkContainer>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
